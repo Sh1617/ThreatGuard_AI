f8a4fbe3e867217249deb71fdb84c0d759b89e01
Sh1617 <22f3001953@ds.study.iitm.ac.in>
2026-05-24 17:17:48 +0530

    frontend setup implimented

--- backend/inference.py
+++ backend/inference.py
@@ -13,12 +13,28 @@ encoder = joblib.load(
 )
 
 
+
+EXPECTED_COLUMNS = model.get_booster().feature_names
+
+
 def predict_attack(data: dict):
 
+    
     input_df = pd.DataFrame([data])
 
+    
+    for col in EXPECTED_COLUMNS:
+
+        if col not in input_df.columns:
+            input_df[col] = 0
+
+    
+    input_df = input_df[EXPECTED_COLUMNS]
+
+    
     prediction = model.predict(input_df)
 
+    
     attack = encoder.inverse_transform(
         prediction
     )[0]
